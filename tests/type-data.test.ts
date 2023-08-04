020440cb44e66a222dfc58961488aa6724057f6a
laludonisetiawan <laludonisetiawan@gmail.com>
2023-08-04 23:02:34 +0800

    type data primitif pada ts

---
+++ tests/type-data.test.ts
@@ -0,0 +1,12 @@
+describe('type data primitif', function ():void {
+    it('harus di deklarasi',function():void {
+        const name: string = "lalu doni setiawan";
+        const balance: number = 12345678;
+        const isVip: boolean = true;
+
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+
+    });
+})
